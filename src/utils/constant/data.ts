fa2e3cf53e0004308695cd81424197f4b9ee2c26
abiyyu03 <abiyyu652@gmail.com>
2023-12-01 13:44:14 +0700

    update : add content

--- src/utils/constant/data.ts
+++ src/utils/constant/data.ts
@@ -2,6 +2,7 @@ interface DataType {
     id: string;
     title: string;
     slug: string;
+    author: string;
     desc: string;
     content: string;
 }
@@ -10,6 +11,7 @@ const data: DataType[] = [
         id: "asdasd",
         title: "Untuk Seseorang",
         slug: "untuk-seseorang",
+        author:"@lvra.ptr_",
         desc:"Untuk seseorang yang Namanya telah ..",
         content: `Untuk seseorang yang Namanya telah Allah tulis berdampingan dengan namaku di lauhul mahfudz, siapapun kamu, dimana pun kamu berada saat ini, semoga Allah melindungimu, semoga Allah menjagamu dari fitnah akhir zaman, dan semoga Allah menjaga hatimu dari hal-hal yang Allah larang. Semoga Allah juga melindungiku.<br /><br />\n
         Percayalah tidak mudah bagiku dalam menanti, tidak mudah untukku menjaga hati. Ada saja ujian yang datang, yang menggoyahkan keimanan. Iya imanku memang belum sekuat itu, tapi aku akan selalu berusaha untuk menjaga, aku akan selalu berdoa agar Allah menjagaku, menjaga hatiku dari segala hal yang Allah haramkan.<br /><br/>\n
@@ -25,6 +27,7 @@ const data: DataType[] = [
         id: "asda123",
         title: "Bagiku",
         slug: "bagiku",
+        author:"@lvra.ptr_",
         desc:"Bagiku sebuah perjalanan adalah ajang ..",
         content: `Bagiku sebuah perjalanan adalah ajang untuk kita terus bersyukur. Karena setiap waktu dalam perjalanan kita akan bermakna ketika kita berusaha memaknainya. <br/><br/>
 
@@ -33,6 +36,29 @@ const data: DataType[] = [
         Mereka sama, tetap manusia yang bisa merasakan lelah, tapi nyatanya mereka tetap berusaha. Ada seorang ayah yang bekerja keras untuk menafkahi keluarganya, ada seorang ibu yang bekerja membawa anaknya, ada anak muda yang sibuk mencari kerja, ada juga yang sibuk pergi pagi pulang malam untuk menuntut ilmu. Semua sama, memiliki kesibukan tersendiri. <br/><br/>
         
         Semua sama, merasakan lelah tersendiri. Jadi, apa masih mau menyerah? Bukankah dunia ini memang tempatnya lelah?`,
+    },{
+        id: "asda1234",
+        title: "Kegagalan adalah bagian dari perjalanan",
+        slug: "kegagalan-adalah-bagian-dari-perjalanan",
+        author:"@budiyana___",
+        desc:"Manusia lahir tak berdaya, Tumbuh besar dan harus berupaya ..",
+        content: ` 
+        Manusia lahir tak berdaya<br/>
+        Tumbuh besar dan harus berupaya<br/><br/>
+        
+        Setiap manusia pasti pernah mengalami yang namanya kegagalan, bahkan balita pun mengalami kegagalan 
+        ketika ia berlatih untuk bisa berjalan.<br/><br/>
+        
+        Kegagalan adalah bagian dari perjalanan, tanpanya kita mungkin tidak akan pernah menjadi lebih baik. 
+        Sebab dari kegagalan itu lahirlah sebuah pembelajaran.<br/><br/>
+        
+        Orang bilang guru terbaik itu adalah pengalaman. Kita merasakan sendiri, kita mengalaminya sendiri, 
+        kita mengetahui dengan baik apa yang berhasil dan apa yang perlu diperbaiki. 
+        Merasakan kecewa ketika mengalami kegagalan adalah hal yang wajar, tapi manusia yang hebat adalah 
+        manusia yang berani untuk bangkit serta belajar dari kegagalannya.<br/><br/>
+        
+        Di saat kita berhasil belajar dari kegagalan, tentu kita memperbaiki apa yang dulu membuat kita gagal. 
+        Bukan tidak mungkin, setelah kegagalan justru keberhasilanlah yang kita raih. So, jangan takut untuk gagal ya👌`,
     }
 ]
 
